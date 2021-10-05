e90564a4055f99ad84606f1b870e32e8b2d20424
Adithyanlfa <90857126+Adithyanlfa@users.noreply.github.com>
2021-10-05 17:54:45 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,16 +1,16 @@
-const Asena = require('../events');
 
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 
-const axios = require('axios');
 
-const Language = require('../language');
 
-const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
-    var r_text = new Array ();
+
+
+
+
+
+
+
 
     
 
@@ -22,18 +22,18 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     
 
-    var i = Math.floor(1*Math.random())
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Adithyanlfa*
 
-*owner number wa.me/917560831917*
 
-*githublink : https://github.com/Adithyanlfa/RickyMwol
 
-Special thanks to : https://github.com/Appuzlfa/thangamani
 
-`}) 
 
-}))
+
+
+
+
+
+
+
+
